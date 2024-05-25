b1eea99f862d6f5db97f57f6350bf066b3b8be42
Malay Singh <malay4singh@gmail.com>
2024-05-25 15:41:35 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB(process.env.DB_URL);
 
 // -----------------------------------server config
 server.use(express.urlencoded());
-server.use(cors({ origin: 'http://localhost:5173' }));
+server.use(cors({ origin: process.env.CORS_ORI }));
 server.use(express.json());
 
 
